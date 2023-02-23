389b5a4096741d3f579ad7574c7f44bca37c672a
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2023-02-23 10:13:17 +0100

    added separated functions to script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
-console.log("Cześć wędrowny developerze!");
+{
+  const welcome = () => {
+    console.log("Cześć wędrowny developerze! ✌");
+  };
 
-let image = document.querySelector(".js-image");
-
-let btn = document.querySelector(".js-button");
-
-let btnChange = document.querySelector(".js-btnColor");
-
-let bodyColor = document.querySelector(".body");
-
-let pink = document.querySelector(".js-normal");
-
-btn.addEventListener("click", () => {
-    image.remove();
-});
-
-btnChange.addEventListener("click", () => {
+  const toggleBackground = () => {
+    const bodyColor = document.querySelector(".body");
+    const pink = document.querySelector(".js-normal");
     bodyColor.classList.toggle("background");
-    pink.innerText = bodyColor.classList.contains("background") ? "normalne" : "pstrokate";
+    pink.innerText = bodyColor.classList.contains("background")
+      ? "normalne"
+      : "pstrokate";
+  };
 
-});
+  const removeImage = () => {
+    const image = document.querySelector(".js-image");
+    image.remove();
+  };
 
+  const init = () => {
+    const btnChange = document.querySelector(".js-btnColor");
+
+    const button = document.querySelector(".js-button");
+
+    btnChange.addEventListener("click", toggleBackground);
+    button.addEventListener("click", removeImage);
+
+    welcome();
+  };
+
+  init();
+}
